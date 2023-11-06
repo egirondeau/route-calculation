ed9fadead7065f487da5dd7f5fd29764e023a132
etachouzin <etienne.tachouzin64@gmail.com>
2023-11-06 22:39:38 +0100

    Add Docstring

--- src/route_calculation/objects/stop.py
+++ src/route_calculation/objects/stop.py
@@ -9,30 +9,44 @@ class Stop:
         self.name = name
         self.neighbors = neighbors
 
-    def add_neighbor(self, stop: "Stop", distance: float):
-        if not self.is_neighbor(stop):
-            self.neighbors[stop] = distance
-            stop.add_neighbor(self, distance)
+    def add_neighbor(self, neighbor: "Stop", distance: float):
+        """
+        Add a neighbor mutually to a stop.
 
-    def is_neighbor(self, stop: "Stop"):
+        :param neighbor: Stop
+        :param distance: Distance between the neighbor and itself.
+        """
+        if not self.is_neighbor(neighbor):
+            self.neighbors[neighbor] = distance
+            neighbor.add_neighbor(self, distance)
+
+    def is_neighbor(self, stop: "Stop") -> bool:
+        """
+        Check if a stop is a neighbor of this.
+
+        :param stop: The stop to check
+        :return: Boolean
+        """
         return stop in self.neighbors.keys()
 
-    def get_distance_with(self, stop: "Stop"):
-        if self.is_neighbor(stop):
-            return self.neighbors[stop]
-        else:
-            pass  # dijkstra
+    def get_distance_with_neighbor(self, neighbor: "Stop") -> float:
+        """
+        Get the distance between 2 neighbors.
 
-    # def SommetMini(self, D):
+        :param neighbor: Stop
+        :return: Distance between
+        """
+        if self.is_neighbor(neighbor):
+            return self.neighbors[neighbor]
 
 
-def get_stop_with_name(name: str):
+def get_stop_with_name(name: str) -> Stop:
     for stop in stops:
         if stop.name.casefold() == name.casefold():
             return stop
 
 
-def get_stop_with_id(id: int):
+def get_stop_with_id(id: int) -> Stop:
     return stops[id]
 
 
